1248b67d2e068b514108f8f3408b138e7f1f85f4
Stafford Williams <stafford.williams@gmail.com>
2021-11-02 16:07:22 +1100

    tests for I

---
+++ src/Tetromino.test.ts
@@ -0,0 +1,57 @@
+import { TetrominoI } from "./Tetromino";
+
+describe("Tetromino", () => {
+  describe("TetominoI", () => {
+    it("should return correct shape", () => {
+      const tetromino = new TetrominoI();
+      expect(tetromino.getShape()).toEqual([
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ]);
+    });
+    it("should rotate once", () => {
+      const tetromino = new TetrominoI();
+      tetromino.rotate();
+      expect(tetromino.getShape()).toEqual([
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+      ]);
+    });
+    it("should rotate twice", () => {
+      const tetromino = new TetrominoI();
+      tetromino.rotate();
+      tetromino.rotate();
+      expect(tetromino.getShape()).toEqual([
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+      ]);
+    });
+    it("should rotate three times", () => {
+      const tetromino = new TetrominoI();
+      tetromino.rotate();
+      tetromino.rotate();
+      tetromino.rotate();
+      expect(tetromino.getShape()).toEqual([
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+      ]);
+    });
+    it("should rotate four times back to original", () => {
+      const tetromino = new TetrominoI();
+      const beforeRotation = tetromino.getShape();
+      tetromino.rotate();
+      tetromino.rotate();
+      tetromino.rotate();
+      tetromino.rotate();
+      expect(tetromino.getShape()).toEqual(beforeRotation);
+    });
+  });
+});
--- src/Tetromino.ts
+++ src/Tetromino.ts
@@ -34,11 +34,22 @@ export abstract class Tetromino {
     protected _x: number,
     protected _y: number,
     color: string,
-    protected _blocks: Point[]
+    protected _blocks: Point[],
+    private pivot: Point
   ) {
     this._color = PIXI.utils.string2hex(color);
   }
-  rotate() {}
+  rotate() {
+    console.log("before", this._blocks);
+    // rotate 90 degrees about pivot
+    this._blocks = this._blocks.map((block) => {
+      const x = this.pivot.x - block.y + this.pivot.y;
+      const y = this.pivot.y + block.x - this.pivot.x;
+      return { x, y };
+    });
+    console.log("after", this._blocks);
+  }
+
   static getRandomTetromino() {
     const randomTetromino = Math.floor(Math.random() * 7);
     switch (randomTetromino) {
@@ -71,81 +82,136 @@ export abstract class Tetromino {
     this._x = x;
     this._y = y;
   }
+  getShape(): number[][] {
+    const grid = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    this._blocks.forEach((block) => {
+      console.log(grid);
+      grid[block.y][block.x] = 1;
+    });
+    return grid;
+  }
 }
 
 export class TetrominoI extends Tetromino {
   constructor(x = 5, y = 0) {
-    super(x, y, cyan[500], [
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
-      { x: 2, y: 0 },
-      { x: 3, y: 0 },
-    ]);
+    super(
+      x,
+      y,
+      cyan[500],
+      [
+        { x: 0, y: 1 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 3, y: 1 },
+      ],
+      { x: 1.5, y: 1.5 }
+    );
   }
 }
 
 export class TetrominoJ extends Tetromino {
   constructor(x = 5, y = 0) {
-    super(x, y, blue[500], [
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
-      { x: 2, y: 0 },
-      { x: 0, y: -1 },
-    ]);
+    super(
+      x,
+      y,
+      blue[500],
+      [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 0, y: -1 },
+      ],
+      { x: 1.5, y: 1.5 }
+    );
   }
 }
 
 export class TetrominoL extends Tetromino {
   constructor(x = 5, y = 0) {
-    super(x, y, orange[500], [
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
-      { x: 2, y: 0 },
-      { x: 2, y: -1 },
-    ]);
+    super(
+      x,
+      y,
+      orange[500],
+      [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 2, y: -1 },
+      ],
+      { x: 1.5, y: 1.5 }
+    );
   }
 }
 
 export class TetrominoO extends Tetromino {
   constructor(x = 5, y = 0) {
-    super(x, y, yellow[500], [
-      { x: 0, y: 0 },
-      { x: 0, y: -1 },
-      { x: 1, y: 0 },
-      { x: 1, y: -1 },
-    ]);
+    super(
+      x,
+      y,
+      yellow[500],
+      [
+        { x: 0, y: 0 },
+        { x: 0, y: -1 },
+        { x: 1, y: 0 },
+        { x: 1, y: -1 },
+      ],
+      { x: 2.5, y: 1.5 }
+    );
   }
 }
 
 export class TetrominoS extends Tetromino {
   constructor(x = 5, y = 0) {
-    super(x, y, green[500], [
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
-      { x: 1, y: -1 },
-      { x: 2, y: -1 },
-    ]);
+    super(
+      x,
+      y,
+      green[500],
+      [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 1, y: -1 },
+        { x: 2, y: -1 },
+      ],
+      { x: 1.5, y: 1.5 }
+    );
   }
 }
 
 export class TetrominoT extends Tetromino {
   constructor(x = 5, y = 0) {
-    super(x, y, purple[500], [
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
-      { x: 2, y: 0 },
-      { x: 1, y: -1 },
-    ]);
+    super(
+      x,
+      y,
+      purple[500],
+      [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 1, y: -1 },
+      ],
+      { x: 1.5, y: 1.5 }
+    );
   }
 }
 
 export class TetrominoZ extends Tetromino {
   constructor(x = 5, y = 0) {
-    super(x, y, red[500], [
-      { x: 0, y: -1 },
-      { x: 1, y: -1 },
-      { x: 1, y: 0 },
-      { x: 2, y: 0 },
-    ]);
+    super(
+      x,
+      y,
+      red[500],
+      [
+        { x: 0, y: -1 },
+        { x: 1, y: -1 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+      ],
+      { x: 1.5, y: 1.5 }
+    );
   }
 }
